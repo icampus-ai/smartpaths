500d78457f48d92e4abcf622dd768b732a0e6f2d
Bhargavi Sreenivasamurthy <icampus.aiagents@gmail.com>
2024-12-25 16:16:05 -0600

    Add files via upload

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,24 @@
+import { Header } from "../sections/Header";
+import { Hero } from "../sections/Hero";
+// import { LogoTicker } from "@/sections/LogoTicker"; 
+import { ProductShowcase } from "@/sections/ProductShowcase";
+import { ProductFeedback } from "@/sections/ProductFeedback"; 
+import { Pricing } from "@/sections/Pricing";
+import { CallToAction } from "@/sections/CallToAction"; 
+import { Footer } from "@/sections/Footer"; 
+
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      {/* <LogoTicker /> */}
+      <ProductShowcase />
+      <ProductFeedback /> 
+      <Pricing />
+      <CallToAction />
+      <Footer />
+    </>
+  );
+}
