bf0b3b650bf019a7e37b2cc8218037f798ef79ed
BhargaviSMurthy <40498171+BhargaviSMurthy@users.noreply.github.com>
2024-12-26 01:50:32 +0100

    auth changes

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -1,70 +1,52 @@
-from flask import url_for, redirect, session, request
-from flask_oauthlib.client import OAuth
+from authlib.integrations.flask_client import OAuth
+from flask import url_for, request, redirect, session
 
 oauth = OAuth()
 
-google = oauth.remote_app(
-    'google',
-    consumer_key='YOUR_GOOGLE_CLIENT_ID',
-    consumer_secret='YOUR_GOOGLE_CLIENT_SECRET',
-    request_token_params={
-        'scope': 'email',
-    },
-    base_url='https://www.googleapis.com/oauth2/v1/',
-    request_token_url=None,
-    access_token_method='POST',
-    access_token_url='https://accounts.google.com/o/oauth2/token',
+# Configure Google OAuth
+google = oauth.register(
+    name='google',
+    client_id='YOUR_GOOGLE_CLIENT_ID',
+    client_secret='YOUR_GOOGLE_CLIENT_SECRET',
     authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    refresh_token_url=None,
+    redirect_uri='http://localhost:5000/auth/google/callback',
+    client_kwargs={'scope': 'profile email'}
 )
 
-github = oauth.remote_app(
-    'github',
-    consumer_key='YOUR_GITHUB_CLIENT_ID',
-    consumer_secret='YOUR_GITHUB_CLIENT_SECRET',
-    request_token_params={
-        'scope': 'user:email',
-    },
-    base_url='https://api.github.com/',
-    request_token_url=None,
-    access_token_method='POST',
-    access_token_url='https://github.com/login/oauth/access_token',
+# Configure GitHub OAuth
+github = oauth.register(
+    name='github',
+    client_id='YOUR_GITHUB_CLIENT_ID',
+    client_secret='YOUR_GITHUB_CLIENT_SECRET',
     authorize_url='https://github.com/login/oauth/authorize',
+    authorize_params=None,
+    access_token_url='https://github.com/login/oauth/access_token',
+    access_token_params=None,
+    refresh_token_url=None,
+    redirect_uri='http://localhost:5000/auth/github/callback',
+    client_kwargs={'scope': 'user:email'}
 )
 
 def login_google():
-    return google.authorize(callback=url_for('auth_google_callback', _external=True))
-
-def login_github():
-    return github.authorize(callback=url_for('auth_github_callback', _external=True))
+    redirect_uri = url_for('auth.auth_google_callback_route', _external=True)
+    return google.authorize_redirect(redirect_uri)
 
 def auth_google_callback():
-    response = google.authorized_response()
-    if response is None or response.get('access_token') is None:
-        return 'Access denied: reason={} error={}'.format(
-            request.args['error_reason'],
-            request.args['error_description']
-        )
+    token = google.authorize_access_token()
+    resp = google.get('userinfo')
+    user_info = resp.json()
+    return user_info
 
-    session['google_token'] = (response['access_token'], '')
-    user_info = google.get('userinfo')
-    return user_info.data
+def login_github():
+    redirect_uri = url_for('auth.auth_github_callback_route', _external=True)
+    return github.authorize_redirect(redirect_uri)
 
 def auth_github_callback():
-    response = github.authorized_response()
-    if response is None or response.get('access_token') is None:
-        return 'Access denied: reason={} error={}'.format(
-            request.args['error_reason'],
-            request.args['error_description']
-        )
-
-    session['github_token'] = (response['access_token'], '')
-    user_info = github.get('user')
-    return user_info.data
-
-@google.tokengetter
-def get_google_oauth_token():
-    return session.get('google_token')
-
-@github.tokengetter
-def get_github_oauth_token():
-    return session.get('github_token')
+    token = github.authorize_access_token()
+    resp = github.get('user')
+    user_info = resp.json()
+    return user_info
