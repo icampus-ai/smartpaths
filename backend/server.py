4476925e715a822f032b7c6cfe2d93bb35f8868c
BhargaviSMurthy <40498171+BhargaviSMurthy@users.noreply.github.com>
2025-02-09 00:41:54 +0100

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -20,5 +20,5 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='thesmartpaths', port=8000, debug=False)
+    app.run(host='137.184.95.180', port=8000, debug=False)
 
